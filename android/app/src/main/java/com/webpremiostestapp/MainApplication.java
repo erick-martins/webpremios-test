4c522a840fe43bfdeae09397c9d24485e3448640
Erick Martins <ericknmp@gmail.com>
2018-10-15 15:22:49 -0300

    Android setup

--- android/app/src/main/java/com/webpremiostestapp/MainApplication.java
+++ android/app/src/main/java/com/webpremiostestapp/MainApplication.java
@@ -3,7 +3,6 @@ package com.webpremiostestapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnativenavigation.NavigationReactPackage;
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new NavigationReactPackage(),
             new ReactNativeLocalizationPackage(),
             new ReactNativeConfigPackage()
       );
